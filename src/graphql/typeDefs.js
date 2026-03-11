58f104e9b08d10a26bfdca8060e91955943fcf7c
tejpal singh <tej@dhwaniastro.com>
2026-03-11 14:45:01 +0530

    change for me

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -157,6 +157,7 @@ type Intake {
   getUsersDetails(page: Int, limit: Int, search: String): UserPagination!
   getAstrologerListBySearch(searchInput: AstrologerSearchInput): AstrologerPagination!
   getRechargePacks: RechargePackResponse!
+  me: User
   }
 
   type Mutation {
