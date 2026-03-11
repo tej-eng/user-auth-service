0b675698e8586b016b25b430fee055fc85a2ce11
tejpal singh <tej@dhwaniastro.com>
2026-03-11 11:37:06 +0530

    change send otp

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,5 +1,5 @@
 function generateOtp() {
-  return Math.floor(100000 + Math.random() * 900000).toString();
+ return Math.floor(1000 + Math.random() * 9000).toString();
 }
 
 function otpExpiryTime() {
