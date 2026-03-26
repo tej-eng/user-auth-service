4d6b56715be778b068efe834b1cbb4e874980944
tejpal singh <tej@dhwaniastro.com>
2026-03-26 12:02:13 +0530

    change redis conn

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const Redis = require("ioredis");
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST ,
-  port: process.env.REDIS_PORT 
-  // password: "your_password", // if needed
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USERNAME,   
+  password: process.env.REDIS_PASSWORD,   
+  
 });
 
 redis.on("connect", () => {
