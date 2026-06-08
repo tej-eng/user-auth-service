4ee9e4a8103cca8196bec24fd2b0fc509ee1c370
tejpal singh <tej@dhwaniastro.com>
2026-06-08 12:55:40 +0530

    change for redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,16 +5,20 @@ const Redis = require("ioredis");
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  username: process.env.REDIS_USERNAME,   
-  password: process.env.REDIS_PASSWORD,   
-  
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
 });
 
-redis.on("error", (err) => console.error("Redis Error:", err));
-
-(async () => {
-  await redis.connect();
+redis.on("connect", () => {
   console.log("Redis Connected ✅");
-})();
+});
+
+redis.on("ready", () => {
+  console.log("Redis Ready ✅");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis Error:", err);
+});
 
 module.exports = redis;
