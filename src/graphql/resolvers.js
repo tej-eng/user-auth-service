61c09ca3e439e08381579229226213585355a81c
tejpal singh <tej@dhwaniastro.com>
2026-07-01 13:14:50 +0530

    change for logs

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -3063,7 +3063,7 @@ module.exports = {
             coins: pack.coins,
             couponCode: coupon?.code || "",
             discount: discount.toString(),
-            type:coupon.type
+            type:coupon?.type ?? "NORMAL",
           },
         });
 
